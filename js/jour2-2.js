38d28fa9e7d791283b43f9b5a7d0d26578b1ffa1
tppro <tristan@plumet.com>
2023-03-29 22:14:08 +0200

    first commit

---
+++ js/jour2-2.js
@@ -0,0 +1,18 @@
+$(function() {
+    $("div.owl-carousel").owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+});
